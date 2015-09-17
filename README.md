f95e9c0458ad8f1679272892f4023df525d9077b
Marcos <velozmarkdrea@gmail.com>
2015-09-17 18:45:28 +0200

    Pushing to repo

---
+++ README.md
@@ -0,0 +1,13 @@
+#Malendar
+#### A calendar tool to manage calendar courses in a natural way
+
+- Private app with 2 profiles: coordinator and/or admin
+- Historic timeline from courses
+- Customization for every course (config file)
+- Entry forms for every single day. Also file uploads and associations
+- PDF output
+- Export/import functionality
+
+License
+-----------------
+Please, read the license.
---
+++ app/var/cache/twig/d/e/debda9e3d627d7237b37b0b2bc531e65016899a847cd2055a9ac67ffbe9d3433.php
@@ -0,0 +1,55 @@
+<?php
+
+/* index.html */
+class __TwigTemplate_1f5f377f22c0ce5d7541c5eed873de084af4a7ea8fa6a3e7b48949f1bc533d04 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("layout.html", "index.html", 1);
+        $this->blocks = array(
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "layout.html";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_content($context, array $blocks = array())
+    {
+        // line 4
+        echo "    Welcome to your new Silex Application!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "index.html";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 4,  28 => 3,  11 => 1,);
+    }
+}
+/* {% extends "layout.html" %}*/
+/* */
+/* {% block content %}*/
+/*     Welcome to your new Silex Application!*/
+/* {% endblock %}*/
+/* */
