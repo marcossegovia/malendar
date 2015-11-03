568cebf3eafc9205718e277c0cd5bdb719d53780
Marcos <velozmarkdrea@gmail.com>
2015-11-03 15:46:48 +0100

    fixing type text and adding phpdoc

--- src/Domain/Entities/Course/Course.php
+++ src/Domain/Entities/Course/Course.php
@@ -6,10 +6,26 @@ namespace Malendar\Domain\Entities\Course;
 
 class Course
 {
+    /**
+     * @var int
+     */
     private $id;
+
+    /**
+     * @var Datetime
+     */
     private $date_start;
+
+    /**
+     * @var Datetime
+     */
     private $date_end;
+
+    /**
+     * @var array
+     */
     private $class_days;
+
     private $master;
 
     public function __construct()
--- src/Domain/Entities/Master/Master.php
+++ src/Domain/Entities/Master/Master.php
@@ -6,10 +6,26 @@ namespace Malendar\Domain\Entities\Master;
 
 class Master
 {
+    /**
+     * @var int
+     */
     private $id;
+
+    /**
+     * @var string
+     */
     private $name;
+
+    /**
+     * @var string
+     */
     private $acronym;
+
+    /**
+     * @var text
+     */
     private $description;
+
     private $courses;
 
     public function __construct()
