4f354f313818955ec72beb35ea6fb4c83d799eb3
Marcos <velozmarkdrea@gmail.com>
2015-11-03 12:35:22 +0100

    fixing yamls, master & course tables are properly formated

--- src/Domain/Entities/Course/Course.php
+++ src/Domain/Entities/Course/Course.php
@@ -6,5 +6,14 @@ namespace Malendar\Domain\Entities\Course;
 
 class Course
 {
+    private $id;
+    private $date_start;
+    private $date_end;
+    private $class_days;
+    private $master;
 
+    public function __construct()
+    {
+
+    }
 }
--- src/Domain/Entities/Master/Master.php
+++ src/Domain/Entities/Master/Master.php
@@ -6,5 +6,14 @@ namespace Malendar\Domain\Entities\Master;
 
 class Master
 {
+    private $id;
+    private $name;
+    private $acronym;
+    private $description;
+    private $courses;
 
+    public function __construct()
+    {
+
+    }
 }
