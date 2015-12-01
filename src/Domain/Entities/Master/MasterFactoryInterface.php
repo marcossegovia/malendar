0c961f960b3648a9e21b20aa9eb412a0d825057d
Marcos <velozmarkdrea@gmail.com>
2015-12-01 21:02:42 +0100

    adding master repository and master factory

---
+++ src/Domain/Entities/Master/MasterFactoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Malendar\Domain\Entities\Master;
+
+
+interface MasterFactoryInterface
+{
+    public static function create($uuid, $name, $acronym, $description);
+}
---
+++ src/Infrastructure/Factory/MasterFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Malendar\Infrastructure\Factory;
+
+
+use Malendar\Domain\Entities\Master\Master;
+use Malendar\Domain\Entities\Master\MasterFactoryInterface;
+
+class MasterFactory implements MasterFactoryInterface
+{
+
+    public static function create($uuid, $name, $acronym, $description)
+    {
+        return new Master($uuid, $name, $acronym, $description);
+    }
+}
