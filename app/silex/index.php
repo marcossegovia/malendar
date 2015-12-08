dac21c3af45bfa6ccada0120ec9ddb484e2be6fe
Marcos <velozmarkdrea@gmail.com>
2015-12-08 14:04:48 +0100

    delegating controllers from index.php to Controllers.php

--- app/silex/index.php
+++ app/silex/index.php
@@ -1,9 +1,5 @@
 <?php
 
-use Malendar\Application\Service\User\LoginUserService;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -12,79 +8,6 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 $app = \Malendar\Infrastructure\Ui\Silex\Application::boostrap();
 
-// Register routes
-$app->get('/', function () use ($app) {
-    if (null === $app['session']->get('user')) {
-        return $app->redirect($app["url_generator"]->generate("login"));
-    }
-    return $app->redirect($app["url_generator"]->generate("dashboard"));
-});
-
-$app->get('/login', function () use ($app) {
-    return new Response($app['twig']->render('login.twig', ['formError' => false]), 200);
-})->bind('login');
-
-$app->post('/login', function (Request $request) use ($app) {
-
-    $commandBus = $app['commandBus'];
-
-    try {
-        $loginService = new \Malendar\Application\Service\User\LoginUserService($commandBus);
-        $loginService->execute($request);
-    } catch (Exception $e) {
-        return new Response($app['twig']->render('login.twig', ['formError' => true]), 400);
-    }
-    return $app->redirect($app["url_generator"]->generate("dashboard"));
-});
-
-$app->get('/logout', function (Request $request) use ($app) {
-
-    $session = $app['session'];
-    $logoutService = new \Malendar\Application\Service\User\LogoutUserService($session);
-
-    $logoutService->execute($request);
-
-    return $app->redirect($app["url_generator"]->generate("login"));
-
-})->bind('logout');
-
-$app->get('/dashboard', function(Request $request) use ($app) {
-    if (null === $app['session']->get('user')) {
-        return $app->redirect($app["url_generator"]->generate("login"));
-    }
-    return new Response($app['twig']->render('dashboard.twig', ['user' => $app['session']->get('user')]), 200);
-})->bind('dashboard');
-
-$app->get('/calendar', function () use ($app) {
-    if (null === $app['session']->get('user')) {
-        return $app->redirect($app["url_generator"]->generate("login"));
-    }
-    return new Response($app['twig']->render('calendar.twig'), 200);
-})->bind('calendar');
-
-
-
-$app->error(function (\Exception $e, $code) use ($app) {
-
-    echo $e->getMessage();
-    if ($app['debug']) {
-        return;
-    }
-    if ($code == 404) {
-        return new Response($app['twig']->render('errors/404_e.twig'), $code);
-    }
-    if ($code == 500) {
-
-        return new Response($app['twig']->render('errors/500.twig'), $code);
-    }
-    if (substr($code, 0, 1) == 4) {
-        return new Response($app['twig']->render('errors/4xx.twig'), $code);
-    }
-    if (substr($code, 0, 1) == 5) {
-        return new Response($app['twig']->render('errors/5xx.twig'), $code);
-    }
-
-    return new Response($app['twig']->render('errors/default.twig'), $code);
-});
+require __DIR__ . '/../../src/Infrastructure/Ui/Silex/Controllers.php';
 
 $app->run();
